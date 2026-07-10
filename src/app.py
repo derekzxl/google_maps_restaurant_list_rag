c69ca168d3a55d8ee8f65e5be39bccfa72bd4511
Derek Zhou <zxl.derek@gmail.com>
2026-07-10 10:49:01 -0500

    Set up render.yaml for hosting

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ Run with: python src/app.py
 
 from flask import Flask, request, jsonify, render_template_string
 from src.query import answer_query
+import os
 
 app = Flask(__name__)
 
@@ -78,4 +79,5 @@ def search():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
